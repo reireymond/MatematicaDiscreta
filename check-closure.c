ed6afc26b8d4ea0151b4742647961eaa60d8777d
Kaua Teixeira <68856326+reireymond@users.noreply.github.com>
2025-11-19 11:45:18 +0000

    comentarios

---
+++ check-closure.c
@@ -0,0 +1,18 @@
+/*
+ * INSTITUTO FEDERAL DE MINAS GERAIS - CAMPUS FORMIGA
+ * Bacharelado em Ciência da Computação
+ * Disciplina: Matemática Discreta
+ * Professor: Diego Mello da Silva
+ *
+ * Trabalho Prático: Verificação de Propriedades e Cálculo de Fechos
+ *
+ * Integrantes do Grupo:
+ * 1. Kaua Teixeira Nascimento - Matrícula: 0117182
+ * 2. Gabriel Mendonça de Oliveira - Matrícula: 0117251
+ * 3. Felipe de Castro Leal Ribeiro - Matrícula: 0117037
+ * 4. Sofia Maria Jesus Leal - Matrícula: 0120376
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
